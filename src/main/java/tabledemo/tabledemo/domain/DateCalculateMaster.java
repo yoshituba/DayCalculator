e4927532d9dd74e0f9ff22671bcca79e32f90a24
Yoshi <aaa465abc.jj.wf@gmail.com>
2018-11-28 01:28:27 +0900

    deleted Setter of Id from DateCalculateMaster

--- src/main/java/tabledemo/tabledemo/domain/DateCalculateMaster.java
+++ src/main/java/tabledemo/tabledemo/domain/DateCalculateMaster.java
@@ -35,10 +35,6 @@ public class DateCalculateMaster {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getDateId() {
         return dateId;
     }
