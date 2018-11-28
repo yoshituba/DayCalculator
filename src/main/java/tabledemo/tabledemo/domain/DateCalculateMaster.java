0858af0af827dfc1242a0657840b5376a94fd5f7
Yoshi <aaa465abc.jj.wf@gmail.com>
2018-11-29 02:17:35 +0900

    revert setId of DateCalculateMaster

--- src/main/java/tabledemo/tabledemo/domain/DateCalculateMaster.java
+++ src/main/java/tabledemo/tabledemo/domain/DateCalculateMaster.java
@@ -35,6 +35,10 @@ public class DateCalculateMaster {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getDateId() {
         return dateId;
     }
