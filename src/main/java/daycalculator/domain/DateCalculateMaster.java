36eed10c964b1c1ff9dc24992ff4ed97ce176377
Yoshi <aaa465abc.jj.wf@gmail.com>
2018-12-10 23:50:26 +0900

    implemened a case of test code

--- src/main/java/daycalculator/domain/DateCalculateMaster.java
+++ src/main/java/daycalculator/domain/DateCalculateMaster.java
@@ -2,6 +2,7 @@ package daycalculator.domain;
 
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -11,6 +12,7 @@ import javax.validation.constraints.NotNull;
 @Table(name = "date_calculate_master")
 @Getter
 @Setter
+@NoArgsConstructor
 public class DateCalculateMaster {
 
     @Id
--- src/test/java/daycalculator/DayCalculatorTests.java
+++ src/test/java/daycalculator/DayCalculatorTests.java
@@ -1,16 +1,51 @@
 package daycalculator;
 
+import daycalculator.domain.DateCalculateMaster;
+import daycalculator.domain.ResultDate;
+import daycalculator.repository.DateCalculateRepository;
+import daycalculator.service.DateCalculateService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DayCalculatorTests {
 
-	@Test
-	public void contextLoads() {
+	private DateCalculateService service;
+
+	private DateCalculateMaster master;
+
+	private DateCalculateRepository repository;
+
+	@Before
+	public void before() throws Exception{
+		service = new DateCalculateService();
+		master = new DateCalculateMaster();
 	}
+
+	@Test
+	public void 翌日の日付計算式に計算基準日20181208を渡して計算結果が20181209になること() throws Exception {
+		master.setId(Long.valueOf(1));
+		master.setDateId("D01");
+		master.setDateName("翌日");
+		master.setYear(0);
+		master.setMonth(0);
+		master.setDay(1);
+		ResultDate resultDate = new ResultDate(master);
+
+		LocalDate actual = service.calculateResultDate("2018/12/08", resultDate);
+
+		assertThat(actual, is(LocalDate.of(2018,12,9)));
+	}
+
 
 }
